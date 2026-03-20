2447ee9870875d9757d6be41e8e4bf0b7837819f
SamarthgkC99 <143303609+SamarthgkC99@users.noreply.github.com>
2026-03-20 07:10:07 +0530

    Change GitHub token in bot.js
    Updated GitHub token for authentication.

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const http  = require('http');
 // ── CONFIG ────────────────────────────────────────────────────────
 // ── GitHub Database Config ───────────────────────────────
 const GITHUB_USER  = 'SamarthgkC99';
-const GITHUB_TOKEN = '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';
+const GITHUB_TOKEN = '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';
 const GITHUB_REPO  = 'bot-state';
 
 const START_BALANCE    = 10000;
